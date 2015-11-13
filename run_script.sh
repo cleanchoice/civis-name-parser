27ad87c05de73e6781f7ffe7bcf3f7885bf6d887
Matt Klaber <klaber+git@gmail.com>
2015-11-13 15:13:38 -0500

    fixing the script

--- run_script.sh
+++ run_script.sh
@@ -4,9 +4,12 @@
 set -e -x
 
 # Civis Custom Scripts don't drop you in to this directory by default
-cd /app
 
-# Install the app
-npm install --production
+if [ ! -e "package.json" ]; then
+  cd /app
+fi
 
-node start -- "$1" "$2" "$3" --bucket "$4"
+# Install the app quietly
+npm install --production > /dev/null
+
+npm start -- "$1" "$2" "$3" --bucket "$4"
